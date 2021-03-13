ae74d5ff31a55d07ef9ec5d0d10b5527961feb77
Vladislav Bukhtaev <buhtaev2015@mail.ru>
2021-03-13 21:18:03 +0800

    Start Homework #3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,92 @@
 package com.company;
 
-import com.company.MusicPlayers.Equipment;
-import com.company.MusicStorages.Library;
-
 public class Main {
 
     /**
-     * Homework #2
-     * @version 1.1
+     * Homework #3
+     *
+     * @version 1.0
      * @author Bukhtaev Vladislav
-     */
-
-    /*
-        Задание.
-
-        Необходимо разработать музыкальную систему.
-        Существуют несколько звуковоспроизводящих устройств (виниловая вертушка, сд, универсальный плеер и т.д.)
-        Существует несколько носителей музыкальных композиций (пластинка, сд, флешка и т.д)
-        Существуют несколько песен, у которых есть имя исполнителя (группы) и название
-        Звуковоспроизводящее устройство должно выводить в консоль информацию о том,
-        что за устройство воспроизводит песню, ее исполнителя и название.
-        В случае, если устройство не может воспроизвести музыку с требуемого носителя выводить соответствующее сообщение
-
+     *
+     * Задание:
+     *
+     * Определить класс, описывающий координаты шахматной клетки.
+     * Данные класса: компоненты x и y, отсчитываемые от левого нижнего угла.
+     * Все методы, позволяющие установить координаты, в том числе и конструкторы,
+     * должны проверять корректность аргументов и генерировать IllegalArgumentException в случае ошибочных значений.
+     * Переопределить метод toString(), выводящий координаты клетки в формате
+     * <номер колонки в виде буквы от 'a' до 'h'><номер строки, начиная с 1>.
+     * Например, клетка с координатами (1, 1) имеет строковое представление "b2".
      */
 
     public static void main(String[] args) {
 
-        Library.fill();
-        Equipment.fill();
-        Menu.chooseAction();
+        /*
+            Случай с корректными параметрами.
+            Создаем клетку C4 с координатами [2, 3].
+         */
+        try {
+            Square square = new Square(2, 3);
+            System.out.println("Успешно создана клетка " + square + " с координатами [2, 3]");
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        /*
+            Случай с НЕкорректными параметрами и выбросом исключения.
+            Создаем несуществующую клетку с координатами [1, 8].
+         */
+        try {
+            Square square = new Square(1, 8);
+            System.out.println("Никаких исключений выброшено небыло"); // Не выпонится.
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        /*
+            Случай с корректными параметрами.
+            Меняем клетке A1 координаты на [5, 2].
+         */
+        try {
+            Square square = new Square(0, 0);
+            square.setX(5);
+            square.setY(2);
+            System.out.println("Координаты клетки A1 успешно изменены на [5, 2]");
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        /*
+            Случай с НЕкорректными параметрами и выбросом исключения.
+            Меняем клетке A1 координату x на 9.
+         */
+        try {
+            Square square = new Square(0, 0);
+            square.setX(9);
+            System.out.println("Никаких исключений выброшено небыло"); // Не выпонится.
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        /*
+            Случай с НЕкорректными параметрами и выбросом исключения.
+            Меняем клетке A1 координату y на 8.
+         */
+        try {
+            Square square = new Square(0, 0);
+            square.setY(8);
+            System.out.println("Никаких исключений выброшено небыло"); // Не выпонится.
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        // Вывод клетки F4 в консоль.
+        Square square = new Square(1, 1);
+        System.out.println("Клетка с координатами [1, 1] называется " + square);
     }
 }
--- src/com/company/MusicPlayers/AbleToPlay.java
+++
@@ -1,7 +0,0 @@
-package com.company.MusicPlayers;
-
-import com.company.MusicStorages.MusicStorage;
-
-public interface AbleToPlay {
-    void play(MusicStorage musicStorage);
-}
